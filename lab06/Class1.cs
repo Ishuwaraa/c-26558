7d03fac611610c0b8df6a6b9e86264e04ff51b15
Ishuwaraa <115783906+Ishuwaraa@users.noreply.github.com>
2023-07-30 02:20:50 +0530

    Add files via upload

---
+++ lab06/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public void myArray(int size)
+        {
+            int[] array = new int[size];
+
+            for(int i=0; i<size/2; i++)
+            {
+                Console.Write("enter " +(i+1)+  " input: ");
+                int input = int.Parse(Console.ReadLine());
+                array[i * 2] = input;
+                array[i * 2 + 1] = 0;
+                
+            }
+            
+            for(int i=0; i<size; i++)
+            {
+                Console.Write(array[i] +" ");
+                
+            }
+        }
+    }
+}
---
+++ lab06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("enter array size: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Class1 objClass = new Class1();
+            objClass.myArray(size);
+            
+
+            Console.ReadLine();
+        }
+
+    }
+}
